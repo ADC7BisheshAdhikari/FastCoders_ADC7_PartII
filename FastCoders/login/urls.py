aa51e6bd46fb36336dc099e9945be722d741bc13
ravisodari-coder <Np03a180236@heraldcollege.edu.np>
2020-02-09 14:14:27 +0545

    CHANGES

--- FastCoders/login/urls.py
+++ FastCoders/login/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     path("register/",views.register, name="register"),
     path("login/",views.login, name="login"),
-    path("index/logout/",views.logout, name="logout"),
+    path("logout/",views.logout, name="logout"),
     path("login/logout/",views.logout, name="logout")
 ]
 
